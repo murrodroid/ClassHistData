db7e471d5f0a207fcc17cf04e8747225c3f573d9
Lucas Mortensen <lucas37c@gmail.com>
2024-12-20 00:23:08 +0100

    changes train.py name to main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# packages
+import torch
+import torch.nn as nn
+# modules
+from modules.import_data import import_data
+from modules.text_preprocessor import *
+from modules.networks import network_qpidgram
+from modules.training import train_model
+
+train_df,full_df = import_data()
+
+X_tensor,vocab = prepare_combined_tensors(train_df,column='tidy_cod')
+y_tensor, label_encoder = encode_labels(train_df, column='icd10h_code')
+
+X_train, X_test, y_train, y_test = train_test_split_tensors(X_tensor, y_tensor)
+
+train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=32, shuffle=True)
+test_loader = DataLoader(TensorDataset(X_test, y_test), batch_size=32, shuffle=False)
+
+vocab_size = len(vocab)
+output_dim = train_df['icd10h_code'].nunique()
+
+model = network_qpidgram(input_dim=vocab_size, len_output=output_dim)
+
+learning_rate = 0.001
+batch_size = 32
+num_epochs = 32
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+train_model(model, train_loader, criterion, optimizer, num_epochs=num_epochs)
