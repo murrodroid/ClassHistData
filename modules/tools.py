4f6594ce4b7499d00f12407559082f99481da015
Lucas Mortensen <lucas37c@gmail.com>
2025-01-10 15:00:33 +0100

    initializes tools module

---
+++ modules/tools.py
@@ -0,0 +1,10 @@
+def remove_text_after_comma(s):
+    paren_level = 0
+    for i, c in enumerate(s):
+        if c == '(':
+            paren_level += 1
+        elif c == ')':
+            paren_level -= 1
+        elif c == ',' and paren_level == 0:
+            return s[:i]
+    return s
