e48eba18856d5c8528293d6d7b5a09f7d3df3ad5
Lucas Mortensen <lucas37c@gmail.com>
2025-03-13 15:24:30 +0100

    replaces code with new functions validate_models and train_k_folds

--- selection_bias_test.py
+++ selection_bias_test.py
@@ -1,16 +1,16 @@
 # packages
-import torch
-import torch.nn as nn
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import StratifiedKFold
 import os
+from sklearn.preprocessing import StandardScaler
+
 # modules
 from modules.import_data import import_data_random
 from modules.text_preprocessor import *
-from modules.networks import individualized_network
-from modules.training import train_model
 from modules.tools import return_device, undersampling
-from modules.model_evaluation import evaluate_model
+from modules.model_evaluation import validate_models
+from modules.training import train_k_folds
+from modules.networks import individualized_network
+
+
 
 #### hyper-parameters
 learning_rate = 0.0008
@@ -21,6 +21,8 @@ retain_pct = 0.4
 k_folds = 5
 undersampling_scale = 0.4
 ####
+
+
 
 device = return_device()
 
@@ -40,89 +42,27 @@ token_types = [
     {'method': 'word', 'ngram': 0}
 ]
 
+
 for i, train_df in enumerate([random_df, ordered_df]):
+    print(f'K-fold training & evaluation of model {model_names[i]} initiated.')
+
     model_folder = f'trained_models/retain_pct_{retain_pct}/{model_names[i]}_{k_folds}Folds'
     os.makedirs(model_folder, exist_ok=True)
     
     print(f'Total size of dataset: {train_df.shape}')
 
-    X_cause, vocab = prepare_deathcauses_tensors(df=train_df, column='deathcause_mono', token_types=token_types)
-    
     scaler_age = StandardScaler().fit(train_df['age'].to_numpy().reshape(-1, 1))
     le_sex = LabelEncoder().fit(train_df['sex'])
     
+    X_cause, vocab = prepare_deathcauses_tensors(df=train_df, column='deathcause_mono', token_types=token_types)
     X_age = torch.tensor(scaler_age.transform(train_df['age'].to_numpy().reshape(-1, 1)), dtype=torch.float).to(device)
     X_sex = torch.tensor(le_sex.transform(train_df['sex']), dtype=torch.long).to(device)
 
     y_column = 'icd10h_random' if i == 0 else 'icd10h_ordered'
     y_tensor, y_label_encoder = encode_labels(train_df, transform_column=y_column, fit_df=df, fit_column='icd10h')
     num_classes = len(y_label_encoder.classes_)
+    criterion = nn.CrossEntropyLoss()
+    fold_model_paths = train_k_folds(i,X_cause,X_age,X_sex,y_tensor,model_names,model_folder,vocab,num_classes,dropout_rate,learning_rate,batch_size,num_epochs,individualized_network,k_folds,criterion,device)
     
-    skf = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state=42)
-    fold_model_paths = []
-    
-    # For each fold, create a train/test split and train the model on the fold's training data
-    for fold, (train_idx, fold_test_idx) in enumerate(skf.split(X_cause, y_tensor.cpu().numpy())):
-        print(f'Fold {fold+1} for {model_names[i]} initiated.')
-        
-        X_cause_train = X_cause[train_idx]
-        X_age_train = X_age[train_idx]
-        X_sex_train = X_sex[train_idx]
-        y_train = y_tensor[train_idx]
-        
-        X_cause_fold_test = X_cause[fold_test_idx]
-        X_age_fold_test = X_age[fold_test_idx]
-        X_sex_fold_test = X_sex[fold_test_idx]
-        y_fold_test = y_tensor[fold_test_idx]
-        
-        train_loader, fold_test_loader = create_dataloaders(
-            train=[X_cause_train, X_age_train, X_sex_train, y_train],
-            test=[X_cause_fold_test, X_age_fold_test, X_sex_fold_test, y_fold_test],
-            batch_size=batch_size
-        )
-        model = individualized_network(
-            vocab_size_cause=len(vocab),
-            num_classes=num_classes,
-            dropout_rate=dropout_rate,
-            emb_dim_cause=128,
-            emb_dim_sex=8,
-            emb_dim_age=16
-        ).to(device)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-        
-        print(f'Training of {model_names[i]} in Fold {fold+1} has begun.')
-        train_model(model, train_loader, fold_test_loader, criterion, optimizer, num_epochs, device=device)
-        
-        fold_model_path = os.path.join(model_folder, f"{model_names[i]}_fold{fold+1}.pth")
-        torch.save(model.state_dict(), fold_model_path)
-        fold_model_paths.append(fold_model_path)
-
-    # Prepare external validation data 
-    print(f'Training complete. Initiating validation check.')
-
     val_df = val_random_df if i == 0 else val_ordered_df
-    X_cause_val, _ = prepare_deathcauses_tensors(df=val_df, column='deathcause_mono', token_types=token_types, pretrained_vocab=vocab)
-    X_age_val = torch.tensor(scaler_age.transform(val_df['age'].to_numpy().reshape(-1, 1)),
-                        dtype=torch.float).to(device)
-    X_sex_val = torch.tensor(le_sex.transform(val_df['sex']),
-                        dtype=torch.long).to(device)
-
-    y_val, _ = encode_labels(val_df, transform_column='icd10h', label_encoder=y_label_encoder)
-    val_loader = create_dataloaders(
-        train=[X_cause_val, X_age_val, X_sex_val, y_val],
-        batch_size=batch_size
-    )
-
-    # Evaluate each fold model on the external validation set
-    for i,fold_model_path in enumerate(fold_model_paths):
-        model = individualized_network(
-            vocab_size_cause=len(vocab),
-            num_classes=num_classes,
-            dropout_rate=dropout_rate,
-            emb_dim_cause=128,
-            emb_dim_sex=8,
-            emb_dim_age=16
-        ).to(device)
-        model.load_state_dict(torch.load(fold_model_path))
-        evaluate_model(model, val_loader, criterion, device, model_folder, f'stats_fold{i+1}.txt')
+    validate_models(val_df, fold_model_paths, scaler_age, le_sex, y_label_encoder, token_types, batch_size, num_classes, vocab, dropout_rate, criterion, model_folder, device)
